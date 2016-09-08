3894a7838230407b99ab4e8feed8542f469184af
Karl <fgeekey2014@163.com>
2016-09-08 16:03:13 +0800

    删除多余文件

---
+++ app/src/main/java/com/heapot/qianxun/adapter/NotificationAdapter.java
@@ -0,0 +1,40 @@
+package com.heapot.qianxun.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.heapot.qianxun.fragment.NotificationFragment;
+import com.heapot.qianxun.fragment.PageFragment;
+
+import java.util.List;
+
+/**
+ * Created by Karl on 2016/8/29.
+ */
+public class NotificationAdapter extends FragmentPagerAdapter {
+    private List<String> mList;
+    private Context mContext;
+
+    public NotificationAdapter(FragmentManager fm, Context context, List<String> list) {
+        super(fm);
+        this.mContext = context;
+        this.mList = list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return NotificationFragment.newInstance(position+1);
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mList.get(position).toString();
+    }
+}
