36f35e341e7c53ee352c31397654219078a0a66d
coderfan <fgeekey2014@163.com>
2016-10-07 14:36:06 +0800

    搭建替代版本编辑区域

--- app/src/main/java/com/heapot/qianxun/activity/CreateArticleActivity.java
+++ app/src/main/java/com/heapot/qianxun/activity/CreateArticleActivity.java
@@ -14,5 +14,6 @@ public class CreateArticleActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_articles);
+
     }
 }
