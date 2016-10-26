1206bc0614d15f7463a5c645b3dee2fa5c220db1
coderfan <fgeekey2014@163.com>
2016-10-26 10:47:50 +0800

    一个特别的注释

--- app/src/main/java/com/heapot/qianxun/activity/MainActivity.java
+++ app/src/main/java/com/heapot/qianxun/activity/MainActivity.java
@@ -61,6 +61,18 @@ import io.rong.imkit.RongIM;
 import io.rong.imlib.RongIMClient;
 import io.rong.imlib.model.UserInfo;
 
+/**
+ * 写字楼里写字间，写字间里程序员；
+ * 程序人员写程序，又拿程序换酒钱。
+ * 酒醒只在网上坐，酒醉还来网下眠；
+ * 酒醉酒醒日复日，网上网下年复年。
+ * 但愿老死电脑间，不愿鞠躬老板前；
+ * 奔驰宝马贵者趣，公交自行程序员。
+ * 别人笑我忒疯癫，我笑自己命太贱；
+ * 不见满街漂亮妹，哪个归得程序员？
+ *
+ */
+
 public class MainActivity extends BaseActivity implements View.OnClickListener, RongIM.UserInfoProvider {
     private DrawerLayout mDrawerLayout;
     private Toolbar mToolBar;
